38ec9b91cc3bdf627f4072e2ba5902b3b62fe7ab
iamvk1437k <automatecode@gmail.com>
2022-12-30 06:24:28 +0530

    All tmp Files Added

---
+++ contacts/Generate.py
@@ -0,0 +1,7 @@
+lft="<li><a href='tel:"
+num=input("Enter Number")
+mid="'>"
+name=input("Enter Name")
+rgt="</a></li>"
+s=lft+num+mid+name+rgt
+print(s)
---
+++ webapp/piped.js
@@ -0,0 +1,5 @@
+function openWin() {
+      window.open("https://piped.kavin.rocks","_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+
+}
+openWin()
